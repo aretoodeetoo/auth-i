33832429a8a35eef1de1ad42cb33ba2bc220c141
croy <roy.christie95@gmail.com>
2019-02-25 17:15:11 -0500

    Built users table

---
+++ migrations/20190225171345_users.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('users', users => {
+        users.increments();
+        users
+            .string('username', 155)
+            .notNullable()
+            .unique();
+        users.string('password', 155).notNullable();
+    });
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('users');
+  
+};
