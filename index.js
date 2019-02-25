ad9f5d5ae535a784be28c109f5e534045ccb4061
croy <roy.christie95@gmail.com>
2019-02-25 18:29:48 -0500

    Added restriction middleware

--- index.js
+++ index.js
@@ -47,13 +47,34 @@ server.post('/api/login', (req, res) => {
         });
 });
 
-server.get('/api/users', (req, res) => {
-    try {
-        const users = Users.find();
-        res.json(users);
-    } catch(error) {
-        res.send(error);
+function restrict(req, res, next) {
+    const { username, password } = req.headers;
+
+    if (username && password) {
+        Users.findBy({ username })
+            .first()
+            .then(user => {
+                if (user && bcrypt.compareSync(password, user.password)) {
+                    next();
+                } else {
+                    res.status(401).json({ message: 'Invalid username or password'});
+                }
+            })
+            .catch(error => {
+                res.status(500).json({ message: 'Sorry about this - had an unexpected error'});
+            });
+    } else {
+        res.status(400).json({ message: 'No username or password input provided '});
     }
+}
+
+server.get('/api/users', restrict, (req, res) => {
+    Users
+        .find()
+        .then(users => {
+            res.json(users);
+        })
+        .catch(err => res.send(err));
 })
 
 const port = 5000;
